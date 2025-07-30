bd1b5ce9b9409b1f04826692f07448f4d1e9cd48
Nicholas <91276870+NicholasCloud4@users.noreply.github.com>
2025-07-30 14:17:09 -0400

    seedTable

--- logTable.js
+++ logTable.js
@@ -4,13 +4,17 @@ import path from 'node:path'
 
 async function viewAllProducts() {
   const db = await open({
-    filename: path.join('database.db'), 
+    filename: path.join('database.db'),
     driver: sqlite3.Database
   });
 
   try {
     const products = await db.all('SELECT * FROM products')
-    console.table(products) 
+    // Neater table display
+    const displayItems = products.map(({ id, title, artist, year, stock }) => {
+      return { id, title, artist, year, stock }
+    })
+    console.table(displayItems)
   } catch (err) {
     console.error('Error fetching products:', err.message)
   } finally {
