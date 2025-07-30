3425add4166e0dabb45fd365e19b2695182f88d6
Nicholas <91276870+NicholasCloud4@users.noreply.github.com>
2025-07-30 14:35:06 -0400

    Getting all products

--- controllers/productsControllers.js
+++ controllers/productsControllers.js
@@ -5,32 +5,45 @@ export async function getGenres(req, res) {
   try {
 
     const db = await getDBConnection()
+
     const genreRows = await db.all('SELECT DISTINCT genre FROM products')
     const genres = genreRows.map(row => row.genre)
     res.json(genres)
-
-    /*
-    Challenge:
-    
-    1. Get all distinct genres (no repeats) from the products table.
-    
-      - Our front end code is expecting an array of genres as strings, but you will likely get an array of objects from the database. Find a solution to that!
-    
-    2. Serve the array of genres and open up the mini browser to check the dropdown is populated.
-    
-    hint.md for help  
-    */
 
   } catch (err) {
 
     res.status(500).json({ error: 'Failed to fetch genres', details: err.message })
 
   }
-
 }
 
-export async function getProducts() {
+export async function getProducts(req, res) {
 
-  console.log('products')
+  try {
+
+    const db = await getDBConnection()
+
+    let query = 'SELECT * FROM products'
+
+    const products = await db.all(query)
+
+    res.json(products)
+
+
+
+    /*
+    Challenge:
+    1. Write logic in getProducts() so all products display on page load.
+       
+       As we will need to modify it in the next challenge, store the SQL query in a let and pass it into the all() method.
+    */
+
+
+
+  } catch (err) {
+
+    res.status(500).json({ error: 'Failed to fetch products', details: err.message })
+
+  }
 
 }
